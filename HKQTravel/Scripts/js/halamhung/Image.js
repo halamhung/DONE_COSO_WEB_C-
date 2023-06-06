89e512dd284c8ac175ef2d56b1987a838a7880a9
halamhung <hungdeptrai111@gmail.com>
2023-06-07 04:01:11 +0700

    first commit

---
+++ HKQTravel/Scripts/js/halamhung/Image.js
@@ -0,0 +1,29 @@
+﻿//function previewImage() {
+//    var preview = document.querySelector('#imgPreview');
+//    var file = document.querySelector('#fileUpLoad').files[0];
+//    var reader = new FileReader();
+
+//    reader.onloadend = function () {
+//        preview.src = reader.result;
+//    }
+
+//    if (file) {
+//        reader.readAsDataURL(file);
+//    } else {
+//        preview.src = "";
+//    }
+//}
+
+fileImage.onchange = evt => {
+
+    const [file] = fileImage.files
+
+    if (file) {
+
+        preview.src = URL.createObjectURL(file);
+
+        $("#preview").removeClass("hidden");
+
+    }
+
+}
